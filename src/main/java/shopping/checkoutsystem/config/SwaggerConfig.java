a91ab050308d06f63e7e56dfe0403b6bfad78561
reivax0z <reivax.norac@gmail.com>
2015-11-02 13:43:17 +1100

    Adding swagger support. Updated the resource context.

---
+++ src/main/java/shopping/checkoutsystem/config/SwaggerConfig.java
@@ -0,0 +1,37 @@
+package shopping.checkoutsystem.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+@Configuration
+@EnableSwagger2
+public class SwaggerConfig {
+    @Bean
+    public Docket api(){
+        return new Docket(DocumentationType.SWAGGER_2)
+                .select()
+                .apis(RequestHandlerSelectors.any())
+                .paths(PathSelectors.any())
+                .build()
+                .pathMapping("/")
+                .apiInfo(apiInfo());
+    }
+
+    private ApiInfo apiInfo() {
+        return new ApiInfo(
+                "Online Checkout System REST API",
+                "This is a description of the exposed services contained in the API.",
+                "0.0.1",
+                "API TOS",
+                "xavier.w.caron@gmail.com",
+                "API License",
+                "API License URL"
+        );
+    }
+}
